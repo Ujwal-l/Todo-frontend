16da0f73f5e62a5adf14ba9de6b4c76fa900966f
Ujwal l <81028495+Ujwal-l@users.noreply.github.com>
2024-06-03 12:14:17 +0530

    Update TaskList.js

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const TaskList = ({ tasks, fetchTasks }) => {
   const markTaskAsDone = async (id) => {
-    await axios.put(`https://todo-backend-2-qke1.onrender.com`, {
+    await axios.put(`https://todo-backend-2-qke1.onrender.com/tasks/:id`, {
       is_done: true,
     });
     fetchTasks();
